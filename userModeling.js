994ddfc385424cd844f14bef0590111b395b126f
Andrew J. Lohr <ajlohr@us.ibm.com>
2015-08-18 14:04:47 -0400

    updates

--- userModeling.js
+++ userModeling.js
@@ -5,21 +5,9 @@ var cfenv = require("cfenv");
 var appEnv = cfenv.getAppEnv();
 var perInsights = appEnv.getService("myPersonalityInsights");
 
-var service_url='';
-var service_username='';
-var service_password='';
-
-if (appEnv.isLocal) {
-  // defaults for dev outside bluemix
-  service_url = 'https://gateway.watsonplatform.net/personality-insights/api';
-  service_username = '42b9703f-2c5a-4f0e-bc16-57c0229dadea';
-  service_password = 'GfO2h2IgA62L';
-} 
-else {
-  service_url = perInsights.credentials.url;
-  service_username = perInsights.credentials.username;
-  service_password = perInsights.credentials.password;
-}
+var service_url = perInsights.credentials.url;
+var service_username = perInsights.credentials.username;
+var service_password = perInsights.credentials.password;
 
 exports.model = function(req, res){
 
